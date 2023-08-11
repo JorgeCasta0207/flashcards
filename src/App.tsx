71d4a839344e89632b371e2898e393d835abbd09
ToasterTecc <toastertechdotnet@gmail.com>
2023-08-11 12:45:27 -0700

    Routes and links added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import './App.css'
-import NavBar from './components/NavBar'
-
 import Home from './routes/Home'
+import Login from './routes/Login'
+import Signup from './routes/Signup'
+import Profile from './routes/Profile'
+import Library from './routes/Library'
+import Study from './routes/Study'
+import SearchResults from './routes/SearchResults'
+import Create from './routes/Create'
+import Edit from './routes/Edit'
+import NotFound from './routes/404'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
-
   return (
-    <>
-    <NavBar/>
-    <Home />
-    </>
+  <BrowserRouter>
+  <Link to ="/">Home |</Link>
+  <Link to ="/Login"> Login |</Link>
+  <Link to ="/Signup"> Signup |</Link>
+  <Link to ="/Profile"> Profile |</Link>
+  <Link to ="/Library"> Library |</Link>
+  <Link to ="/Study"> Study |</Link>
+  <Link to ="/Results"> SearchResults |</Link>
+  <Link to ="/Create"> Create |</Link>
+  <Link to ="/Edit"> Edit |</Link>
+    <Routes>
+
+      <Route path="/" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/Library" element={<Library />} />
+      <Route path="/Study" element={<Study />} />
+      <Route path="/Results" element={<SearchResults />} />
+      <Route path="/Create" element={<Create />}/>
+      <Route path="/Edit" element={<Edit />}/>
+      <Route path='*' element={<NotFound />}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>Login
+      <Link to='/Signup'><p className="text-blue">Link to Sign up</p></Link>
+      {/*why text color no work?*/}
+    </div>
   )
 }
 export default Login
