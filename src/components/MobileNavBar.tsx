81abea494473d33dcae519efb9abebf684b37632
tylerjgreen22 <tylerjgreen22@gmail.com>
2023-08-21 16:20:51 -0700

    changed routing and mobile bg

--- src/components/MobileNavBar.tsx
+++ src/components/MobileNavBar.tsx
@@ -4,52 +4,72 @@ import plus from "../assets/plus.png";
 import search from "../assets/search.png";
 import note from "../assets/note.png";
 
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const MobileNavBar = () => {
-    const Location = useLocation();
-    if (Location.pathname === '/Create' || Location.pathname === '/Profile' || Location.pathname === '/Study' || Location.pathname === '/Edit'){
-    return(
-    <div className="sticky w-full z-50 bottom-0 py-3 absolute bg-primary hidden max-lg:block">
-        <div className="container flex items-center place-content-evenly m-auto" /*style={{ padding-right: ` 5%!important; `, padding-left:` 5%!Important` }}*/ >
+  {
+    return (
+      <div className="sticky w-full z-50 bottom-0 py-3 absolute bg-primary hidden max-lg:block">
+        <div
+          className="container flex items-center place-content-evenly m-auto" /*style={{ padding-right: ` 5%!important; `, padding-left:` 5%!Important` }}*/
+        >
           <div>
-            <Link to ="/">
-            <img src={home} alt="home" className="p-[10px]" style={{width:`70px`, height:`auto` }} />
+            <Link to="/">
+              <img
+                src={home}
+                alt="home"
+                className="p-[10px]"
+                style={{ width: `70px`, height: `auto` }}
+              />
             </Link>
           </div>
-         
-           
-      <div className="block ">
-      <Link to ="/Study">
-       <img src={search} alt="note" className="p-[10px]" style={{width:`70px`, height:`auto` }}/>
-      </Link>
-      </div>
-      
-      <div className="block ">
-      <Link to="/Create">
-      <img src={plus} alt="note" className="p-[5px] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.45)] w-[100px] h-auto -mt-[86%] active:translate-y-2 transition-all duration-150" />
-      </Link>
-      </div>
-      
-          
-      <div className="block ">
-      <Link to="/Edit">
-       <img src={note} alt="note" className="p-[10px]" style={{width:`70px`, height:`auto`}} />
-      </Link>
-      </div>
-          
-      <div className="block ">
+
+          <div className="block ">
+            <Link to="/Study">
+              <img
+                src={search}
+                alt="note"
+                className="p-[10px]"
+                style={{ width: `70px`, height: `auto` }}
+              />
+            </Link>
+          </div>
+
+          <div className="block ">
+            <Link to="/Create">
+              <img
+                src={plus}
+                alt="note"
+                className="p-[5px] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.45)] w-[100px] h-auto -mt-[86%] active:translate-y-2 transition-all duration-150"
+              />
+            </Link>
+          </div>
+
+          <div className="block ">
+            <Link to="/Edit">
+              <img
+                src={note}
+                alt="note"
+                className="p-[10px]"
+                style={{ width: `70px`, height: `auto` }}
+              />
+            </Link>
+          </div>
+
+          <div className="block ">
             <Link to="/Profile">
-             <img src={person} alt="person" className="p-[10px]" style={{width:`70px`, height:`auto`}} />
+              <img
+                src={person}
+                alt="person"
+                className="p-[10px]"
+                style={{ width: `70px`, height: `auto` }}
+              />
             </Link>
-          
           </div>
-          
         </div>
       </div>
-    )} else {
-        return(<></>)
-    }
+    );
+  }
 };
 
-export default MobileNavBar
+export default MobileNavBar;
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,31 @@
+import { RouteObject, createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+import Library from "./Library";
+import Study from "./Study";
+import SearchResults from "./SearchResults";
+import Create from "./Create";
+import Edit from "./Edit";
+import NotFound from "./404";
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/Login", element: <Login /> },
+      { path: "/Signup", element: <Signup /> },
+      { path: "/Profile", element: <Profile /> },
+      { path: "/Library", element: <Library /> },
+      { path: "/Study", element: <Study /> },
+      { path: "/Results", element: <SearchResults /> },
+      { path: "/Create", element: <Create /> },
+      { path: "/Edit", element: <Edit /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
