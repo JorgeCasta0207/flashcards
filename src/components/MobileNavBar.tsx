e76b62c0385cb871d690b5af779eb872e6cf281e
ToasterTecc <toastertechdotnet@gmail.com>
2023-08-30 17:40:52 -0700

    mobile navbar / navbar / app changes

--- src/components/MobileNavBar.tsx
+++ src/components/MobileNavBar.tsx
@@ -1,15 +1,42 @@
 import home from "../assets/home.png";
 import person from "../assets/person.png";
 import plus from "../assets/plus.png";
-import search from "../assets/search.png";
+import searchpic from "../assets/search.png";
 import note from "../assets/note.png";
+import SearchIcon from "@mui/icons-material/Search";
 
 import { Link } from "react-router-dom";
+import { Button, Modal, Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { router } from "../routes/Routes";
 
 const MobileNavBar = () => {
+  const [search, setSearch] = useState("");
+  const [openModal, setOpen] = useState(false);
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
+
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '25%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && search) {
+      router.navigate(`/Results/${search}`);
+    }
+  };
+  
   {
     return (
-      <div className="sticky w-full z-50 bottom-0 py-3 absolute bg-primary hidden max-lg:block">
+      <div className="sticky w-full z-50 bottom-0 py-3 absolute bg-primary hidden max-lg:block drop-shadow-[0px_2px_5px_rgba(0,0,0,0.75)]">
         <div
           className="container flex items-center place-content-evenly m-auto" /*style={{ padding-right: ` 5%!important; `, padding-left:` 5%!Important` }}*/
         >
@@ -25,14 +52,39 @@ const MobileNavBar = () => {
           </div>
 
           <div className="block ">
-            <Link to="/Study">
+            <Button onClick={handleOpenModal}>
               <img
-                src={search}
-                alt="note"
+                src={searchpic}
+                alt="search"
                 className="p-[10px]"
                 style={{ width: `70px`, height: `auto` }}
               />
-            </Link>
+            </Button>
+            {/*modal stuff*/}
+            <Modal
+            open={openModal}
+            onClose={handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h4" component="h2">
+                Search
+              </Typography>
+            <div className="relative w-[100%] mt-2">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+              <SearchIcon />
+            </div>
+            <input
+              className="w-full p-2 pl-10 text-white placeholder:text-white bg-accent rounded-lg"
+              placeholder="Search all sets..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyUp={handleSearch}
+            />
+          </div>
+          </Box>
+          </Modal>
           </div>
 
           <div className="block ">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import { router } from "../routes/Routes";
 import Avatar from "@mui/material/Avatar";
-import { Box, Button, Menu, MenuItem, Modal, Typography } from "@mui/material";
+import { Menu, MenuItem } from "@mui/material";
 import React from "react";
 import agent from "../api/agent";
 import { User } from "../models/user";
@@ -12,22 +12,6 @@ const NavBar = () => {
   const [search, setSearch] = useState("");
   const [user, setUser] = useState<User | null>(null);
   const token = localStorage.getItem("token");
-  const [openModal, setOpen] = React.useState(false);
-  const handleOpenModal = () => setOpen(true);
-  const handleCloseModal = () => setOpen(false);
-
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '25%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 350,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -80,7 +64,7 @@ const NavBar = () => {
 
   return (
     <nav className="bg-primary px-5">
-      <div className="">
+      <div>
         <div className="mx-auto flex justify-between items-center p-1">
           {/*Logo vvv*/}
           <img
@@ -101,7 +85,7 @@ const NavBar = () => {
               <Link to="/Create"> Create |</Link>
               <Link to="/Edit"> Edit |</Link> */}
 
-          <div className="relative w-1/3 visible max-lg:hidden">
+          <div className="relative w-1/3">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3">
               <SearchIcon />
             </div>
@@ -113,38 +97,6 @@ const NavBar = () => {
               onKeyUp={handleSearch}
             />
           </div>
-
-          {/*-- Modal toggle --*/}
-          <Button variant="outlined" startIcon={<SearchIcon sx={{color:'black', fontSize:'1.5rem'}}/>} sx={{ display: { xs: 'visible', lg: 'none'}, color:'white', bgcolor: 'rgb(156 115 246)', ":hover": {bgcolor:'#834AFF'}, textTransform: 'none'}} onClick={handleOpenModal}>
-          <Typography sx={{ fontWeight: 'medium' }}>Search</Typography>
-           </Button>
-          <Modal
-            open={openModal}
-            onClose={handleCloseModal}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h4" component="h2">
-                Search
-              </Typography>
-            <div className="relative w-[100%] mt-2">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3">
-              <SearchIcon />
-            </div>
-            <input
-              className="w-full p-2 pl-10 text-white placeholder:text-white bg-accent rounded-lg"
-              placeholder="Search all sets..."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyUp={handleSearch}
-            />
-          </div>
-          </Box>
-          </Modal>
-
-
-
 
           {user ? (
             <div>
